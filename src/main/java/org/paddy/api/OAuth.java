16e908c76098feb78bfbca0f6c2a473af6ab13a6
Patrick-Emil Zörner <paddyez@gmail.com>
2019-02-06 09:44:25 +0100

    Made more readable

--- src/main/java/org/paddy/api/OAuth.java
+++ src/main/java/org/paddy/api/OAuth.java
@@ -1,45 +1,87 @@
 package org.paddy.api;
+import org.springframework.boot.json.BasicJsonParser;
+import org.springframework.boot.json.JsonParser;
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.stream.Collectors;
 public class OAuth {
-    Map<String, String> configMap;
-    Set<String> servicesS = new HashSet<>(Arrays.asList("authorize", "token", "revoke"));
+    private HttpURLConnection connection;
+    private Map<String, String> configMap;
+    private final Set<String> servicesS = new HashSet<>(Arrays.asList("authorize", "token", "revoke"));
     private final String charset = StandardCharsets.UTF_8.name();
     private final String oauthServices = "/services/oauth2/";
+    private String credentials;
+    private  String bearer;
     public OAuth(Map<String, String> configMap) {
         this.configMap = configMap;
     }
     public void login () {
-        String uri = "https://" + configMap.get("my_domain") +  oauthServices + "token";
+        String response;
+        connect();
+        if (connection == null) throw new AssertionError();
         try {
+            printToOut();
+            connection.connect();
+            //printHeader();
+            response = response();
+            if(response != null) {
+                setBearer(response);
+            }
+        }
+        catch (IOException ioe) {
+            System.err.println("### " + ioe);
+        }
+        //System.out.println(bearer);
+    }
+    private void connect() {
+        /* TODO if other than login */
+        String lengthS = String.valueOf(loginCredentials().length());
+        try {
+            String uri = "https://" + configMap.get("my_domain") + oauthServices + "token";
             URL url = new URL(uri);
-            String content = "grant_type=password";
-            content += "&username=" + configMap.get("username");
-            content += "&password=" + configMap.get("password")+configMap.get("security_token");
-            content += "&client_id=" + configMap.get("client_id");
-            content += "&client_secret=" + configMap.get("client_secret");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
-            connection.setUseCaches( false );
+            connection.setUseCaches(false);
             connection.setDoOutput(true);
             connection.setDoInput(true);
             connection.setRequestProperty("charset", charset);
-            connection.addRequestProperty("Content-Length", String.valueOf(content.length()));
+            connection.addRequestProperty("Content-Length", lengthS);
             connection.setConnectTimeout(5000);
             connection.setReadTimeout(5000);
             //connection.addRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             //connection.addRequestProperty( "Accept", "*/*" );
             //connection.addRequestProperty( "Accept-Encoding", "gzip, deflate, compress" );
-            PrintStream  printStream = new PrintStream(connection.getOutputStream());
-            printStream.print(content);
+        }
+        catch (IOException ioe) {
+            System.err.println("### " + ioe);
+        }
+    }
+    private String loginCredentials() {
+        String credentials = "grant_type=password";
+        credentials += "&username=" + configMap.get("username");
+        credentials += "&password=" + configMap.get("password")+configMap.get("security_token");
+        credentials += "&client_id=" + configMap.get("client_id");
+        credentials += "&client_secret=" + configMap.get("client_secret");
+        return credentials;
+    }
+    private void printToOut() {
+        /* TODO print other than login */
+        credentials = loginCredentials();
+        try {
+            PrintStream printStream = new PrintStream(connection.getOutputStream());
+            printStream.print(credentials);
             printStream.close();
-            connection.connect();
-            //printHeader(connection);
+        }
+        catch (IOException ioe) {
+            System.err.println("### " + ioe);
+        }
+    }
+    private String response() {
+        String response = null;
+        try {
             int status = connection.getResponseCode();
             BufferedReader bufferedReader;
             if (status > 299) {
@@ -52,21 +94,26 @@ public class OAuth {
             while ((line = bufferedReader.readLine()) != null) {
                 stringWriter.append(line);
             }
-            String response = stringWriter.toString();
-            System.out.println(response);
             bufferedReader.close();
-        }
-        catch (MalformedURLException mfe) {
-            System.err.println("### " + mfe);
+            response = stringWriter.toString();
         }
         catch (IOException ioe) {
             System.err.println("### " + ioe);
         }
+        return response;
     }
-    private static void printHeader(HttpURLConnection connection) {
-        Map<String, List<String>> hdrs = connection.getHeaderFields();
+    private void setBearer(String response) {
+        JsonParser jsonParser = new BasicJsonParser();
+        Map jsonMap = jsonParser.parseMap(response);
+        jsonMap.forEach((k, v) -> {
+            if (k.equals("access_token")) {
+                bearer = String.valueOf(v);
+            }
+        });
+    }
+    private void printHeader() {
         System.out.println("----");
-        hdrs.forEach((key,  values) -> {
+        connection.getHeaderFields().forEach((key,  values) -> {
             System.out.print(key + ": ");
             String joined = values.stream().collect(Collectors.joining("|"));
             System.out.println(joined);

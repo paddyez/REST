b1507c9c866ac9a4604cdd7860d9d66b26b38e0b
Patrick-Emil Zörner <paddyez@gmail.com>
2020-04-21 16:40:38 +0200

    ResponseStatusCodes Enum

--- src/main/java/org/paddy/utils/ResponseStatusCodes.java
+++ src/main/java/org/paddy/utils/ResponseStatusCodes.java
@@ -1,82 +1,125 @@
 package org.paddy.utils;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class ResponseStatusCodes {
-    private static final int GET_200 = 200;
-    private static final int PATCH_200 = 200;
-    private static final int PATCH_204 = 204;
-    private static final int DELETE_GET_PATCH_POST_PUT_400 = 400;
-    private static final int DELETE_GET_PATCH_POST_PUT_403 = 403;
-    private static final int DELETE_GET_PATCH_POST_PUT_404_ANNOTATION = 404;
-    private static final int DELETE_GET_PATCH_POST_PUT_404_URL_EXTENSION = 404;
-    private static final int DELETE_GET_PATCH_POST_PUT_404_CLASS_NOT_FOUND = 404;
-    private static final int DELETE_GET_PATCH_POST_PUT_405 = 405;
-    private static final int DELETE_GET_PATCH_POST_PUT_406_CONTENT_TYPE = 406;
-    private static final int DELETE_GET_PATCH_POST_PUT_406_HEADER_NOT_SUPPORTED = 406;
-    private static final int GET_PATCH_POST_PUT_406 = 406;
-    private static final int DELETE_GET_PATCH_POST_PUT_415_XML_NOT_SUPPORTED = 415;
-    private static final int DELETE_GET_PATCH_POST_PUT_415_CONTENT_HEADER_NOT_SUPPORTED = 415;
-    private static final int DELETE_GET_PATCH_POST_PUT_500 = 500;
-    private static final HashMap<String, String> RESPONSE_STATUS_CODES_DESC = new HashMap<>();
-    private static final HashMap<String, Integer> RESPONSE_STATUS_CODES = new HashMap<>();
+    public enum CodeEnum {
+        GET_200(200) {
+            @Override
+            public String getDescription() {
+                return "The request was successful.";
+            }
+        },
+        PATCH_200(200) {
+            @Override
+            public String getDescription() {
+                return "The request was successful and the return type is non-void.";
+            }
+        },
+        PATCH_204(204) {
+            @Override
+            public String getDescription() {
+                return "The request was successful and the return type is void.";
+            }
+        },
+        DELETE_GET_PATCH_POST_PUT_400(400) {
+            @Override
+            public String getDescription() {
+                return "An unhandled user exception occurred.";
+            }
+        },
+        DELETE_GET_PATCH_POST_PUT_403(403) {
+            @Override
+            public String getDescription() {
+                return "You don't have access to the specified Apex class.";
+            }
+        },
+        DELETE_GET_PATCH_POST_PUT_404_ANNOTATION(404) {
+            @Override
+            public String getDescription() {
+                return "The URL is unmapped in an existing @RestResource annotation.";
+            }
+        },
+        DELETE_GET_PATCH_POST_PUT_404_URL_EXTENSION(404) {
+            @Override
+            public String getDescription() {
+                return "The URL extension is unsupported.";
+            }
+        },
+        DELETE_GET_PATCH_POST_PUT_404_CLASS_NOT_FOUND(404) {
+            @Override
+            public String getDescription() {
+                return "The Apex class with the specified namespace couldn't be found.";
+            }
+        },
+        DELETE_GET_PATCH_POST_PUT_405(405) {
+            @Override
+            public String getDescription() {
+                return "The request method doesn't have a corresponding Apex method.";
+            }
+        },
+        DELETE_GET_PATCH_POST_PUT_406_CONTENT_TYPE(406) {
+            @Override
+            public String getDescription() {
+                return "The Content-Type property in the header was set to a value other than JSON or XML.";
+            }
+        },
+        DELETE_GET_PATCH_POST_PUT_406_HEADER_NOT_SUPPORTED(406) {
+            @Override
+            public String getDescription() {
+                return "The header specified in the HTTP request is not supported.";
+            }
+        },
+        GET_PATCH_POST_PUT_406(406) {
+            @Override
+            public String getDescription() {
+                return "The XML return type specified for format is unsupported.";
+            }
+        },
+        DELETE_GET_PATCH_POST_PUT_415_XML_NOT_SUPPORTED(415) {
+            @Override
+            public String getDescription() {
+                return "The XML parameter type is unsupported.";
+            }
+        },
+        DELETE_GET_PATCH_POST_PUT_415_CONTENT_HEADER_NOT_SUPPORTED(415) {
+            @Override
+            public String getDescription() {
+                return "The Content-Header Type specified in the HTTP request header is unsupported.";
+            }
+        },
+        DELETE_GET_PATCH_POST_PUT_500(500) {
+            @Override
+            public String getDescription() {
+                return "An unhandled Apex exception occurred.";
+            }
+        };
 
-    static {
-        RESPONSE_STATUS_CODES_DESC.put("GET_200", "The request was successful.");
-        RESPONSE_STATUS_CODES_DESC.put("PATCH_200", "The request was successful and the return type is non-void.");
-        RESPONSE_STATUS_CODES_DESC.put("PATCH_204", "The request was successful and the return type is void.");
-        RESPONSE_STATUS_CODES_DESC.put("DELETE_GET_PATCH_POST_PUT_400", "An unhandled user exception occurred.");
-        RESPONSE_STATUS_CODES_DESC.put("DELETE_GET_PATCH_POST_PUT_403", "You don't have access to the specified Apex class.");
-        RESPONSE_STATUS_CODES_DESC.put("DELETE_GET_PATCH_POST_PUT_404_ANNOTATION", "The URL is unmapped in an existing @RestResource annotation.");
-        RESPONSE_STATUS_CODES_DESC.put("DELETE_GET_PATCH_POST_PUT_404_URL_EXTENSION", "The URL extension is unsupported.");
-        RESPONSE_STATUS_CODES_DESC.put("DELETE_GET_PATCH_POST_PUT_404_CLASS_NOT_FOUND", "The Apex class with the specified namespace couldn't be found.");
-        RESPONSE_STATUS_CODES_DESC.put("DELETE_GET_PATCH_POST_PUT_405", "The request method doesn't have a corresponding Apex method.");
-        RESPONSE_STATUS_CODES_DESC.put("DELETE_GET_PATCH_POST_PUT_406_CONTENT_TYPE", "The Content-Type property in the header was set to a value other than JSON or XML.");
-        RESPONSE_STATUS_CODES_DESC.put("DELETE_GET_PATCH_POST_PUT_406_HEADER_NOT_SUPPORTED", "The header specified in the HTTP request is not supported.");
-        RESPONSE_STATUS_CODES_DESC.put("GET_PATCH_POST_PUT_406", "The XML return type specified for format is unsupported.");
-        RESPONSE_STATUS_CODES_DESC.put("DELETE_GET_PATCH_POST_PUT_415_XML_NOT_SUPPORTED", "The XML parameter type is unsupported.");
-        RESPONSE_STATUS_CODES_DESC.put("DELETE_GET_PATCH_POST_PUT_415_CONTENT_HEADER_NOT_SUPPORTED", "The Content-Header Type specified in the HTTP request header is unsupported.");
-        RESPONSE_STATUS_CODES_DESC.put("DELETE_GET_PATCH_POST_PUT_500", "An unhandled Apex exception occurred.");
-        RESPONSE_STATUS_CODES.put("GET_200", GET_200);
-        RESPONSE_STATUS_CODES.put("PATCH_200", PATCH_200);
-        RESPONSE_STATUS_CODES.put("PATCH_204", PATCH_204);
-        RESPONSE_STATUS_CODES.put("DELETE_GET_PATCH_POST_PUT_400", DELETE_GET_PATCH_POST_PUT_400);
-        RESPONSE_STATUS_CODES.put("DELETE_GET_PATCH_POST_PUT_403", DELETE_GET_PATCH_POST_PUT_403);
-        RESPONSE_STATUS_CODES.put("DELETE_GET_PATCH_POST_PUT_404_ANNOTATION", DELETE_GET_PATCH_POST_PUT_404_ANNOTATION);
-        RESPONSE_STATUS_CODES.put("DELETE_GET_PATCH_POST_PUT_404_URL_EXTENSION", DELETE_GET_PATCH_POST_PUT_404_URL_EXTENSION);
-        RESPONSE_STATUS_CODES.put("DELETE_GET_PATCH_POST_PUT_404_CLASS_NOT_FOUND", DELETE_GET_PATCH_POST_PUT_404_CLASS_NOT_FOUND);
-        RESPONSE_STATUS_CODES.put("DELETE_GET_PATCH_POST_PUT_405", DELETE_GET_PATCH_POST_PUT_405);
-        RESPONSE_STATUS_CODES.put("DELETE_GET_PATCH_POST_PUT_406_CONTENT_TYPE", DELETE_GET_PATCH_POST_PUT_406_CONTENT_TYPE);
-        RESPONSE_STATUS_CODES.put("DELETE_GET_PATCH_POST_PUT_406_HEADER_NOT_SUPPORTED", DELETE_GET_PATCH_POST_PUT_406_HEADER_NOT_SUPPORTED);
-        RESPONSE_STATUS_CODES.put("GET_PATCH_POST_PUT_406", GET_PATCH_POST_PUT_406);
-        RESPONSE_STATUS_CODES.put("DELETE_GET_PATCH_POST_PUT_415_XML_NOT_SUPPORTED", DELETE_GET_PATCH_POST_PUT_415_XML_NOT_SUPPORTED);
-        RESPONSE_STATUS_CODES.put("DELETE_GET_PATCH_POST_PUT_415_CONTENT_HEADER_NOT_SUPPORTED", DELETE_GET_PATCH_POST_PUT_415_CONTENT_HEADER_NOT_SUPPORTED);
-        RESPONSE_STATUS_CODES.put("DELETE_GET_PATCH_POST_PUT_500", DELETE_GET_PATCH_POST_PUT_500);
+        public static final String NO_DESCRIPTION_AVAILABLE = "No description available";
+        private final int number;
+
+        CodeEnum(int number) {
+            this.number = number;
+        }
+
+        public int getNumber() {
+            return this.number;
+        }
+
+        public String getDescription() {
+            return NO_DESCRIPTION_AVAILABLE;
+        }
     }
 
     private ResponseStatusCodes() {
     }
 
     public static String getPossibleCause(String restMethod, int code) {
-        Set<String> possibleKeysS = getPossibleKeys(restMethod, code);
         StringBuilder possibleCause = new StringBuilder();
-        for (String key : possibleKeysS) {
-            possibleCause.append(RESPONSE_STATUS_CODES_DESC.get(key))
-                    .append("\n");
-        }
-        return possibleCause.toString().trim();
-    }
-
-    private static Set<String> getPossibleKeys(String restMethod, int code) {
-        Set<String> possibleKeysS = new HashSet<>();
-        for (Map.Entry<String, Integer> entry: RESPONSE_STATUS_CODES.entrySet()) {
-            if (entry.getValue() == code && entry.getKey().contains(restMethod)) {
-                possibleKeysS.add(entry.getKey());
+        for (CodeEnum codeEnum : CodeEnum.values()) {
+            if (codeEnum.getNumber() == code && codeEnum.name().contains(restMethod)) {
+                possibleCause.append(codeEnum.getDescription())
+                        .append("\n");
             }
         }
-        return possibleKeysS;
+        return possibleCause.toString().trim();
     }
 }
---
+++ src/test/java/org/paddy/utils/ResponseStatusCodesTest.java
@@ -0,0 +1,87 @@
+package org.paddy.utils;
+
+import org.junit.jupiter.api.Test;
+import org.paddy.utils.ResponseStatusCodes.CodeEnum;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ResponseStatusCodesTest {
+    public static final String EXPECTED_GET_404 = "The URL is unmapped in an existing @RestResource annotation.\n" +
+            "The URL extension is unsupported.\n" +
+            "The Apex class with the specified namespace couldn't be found.";
+    public static final String EXPECTED_GET_406 = "The Content-Type property in the header was set to a value other than JSON or XML.\n" +
+            "The header specified in the HTTP request is not supported.\n" +
+            "The XML return type specified for format is unsupported.";
+    public static final String EXPECTED_GET_415 = "The XML parameter type is unsupported.\n" +
+            "The Content-Header Type specified in the HTTP request header is unsupported.";
+    public static final String EXPECTED_POST_404 = "The URL is unmapped in an existing @RestResource annotation.\n" +
+            "The URL extension is unsupported.\n" +
+            "The Apex class with the specified namespace couldn't be found.";
+    public static final String EXPECTED_POST_406 = "The Content-Type property in the header was set to a value other than JSON or XML.\n" +
+            "The header specified in the HTTP request is not supported.\n" +
+            "The XML return type specified for format is unsupported.";
+    public static final String EXPECTED_POST_415 = "The XML parameter type is unsupported.\n" +
+            "The Content-Header Type specified in the HTTP request header is unsupported.";
+
+    @Test
+    public void testGET() {
+        for (CodeEnum codeEnum : CodeEnum.values()) {
+            int n = codeEnum.getNumber();
+            String possibleCause = ResponseStatusCodes.getPossibleCause("GET", n);
+            if (codeEnum.name().contains("GET")) {
+                if (n == 200) {
+                    assertThat(possibleCause).isEqualTo(CodeEnum.GET_200.getDescription());
+                } else if (n == 400) {
+                    assertThat(possibleCause).isEqualTo(CodeEnum.DELETE_GET_PATCH_POST_PUT_400.getDescription());
+                } else if (n == 403) {
+                    assertThat(possibleCause).isEqualTo(CodeEnum.DELETE_GET_PATCH_POST_PUT_403.getDescription());
+                } else if (n == 404) {
+                    assertThat(possibleCause).isEqualTo(EXPECTED_GET_404);
+                } else if (n == 405) {
+                    assertThat(possibleCause).isEqualTo(CodeEnum.DELETE_GET_PATCH_POST_PUT_405.getDescription());
+                } else if (n == 406) {
+                    assertThat(possibleCause).isEqualTo(EXPECTED_GET_406);
+                } else if (n == 415) {
+                    assertThat(possibleCause).isEqualTo(EXPECTED_GET_415);
+                } else if (n == 500) {
+                    assertThat(possibleCause).isEqualTo(CodeEnum.DELETE_GET_PATCH_POST_PUT_500.getDescription());
+                } else {
+                    assertThat(possibleCause).isEqualTo("");
+                }
+            } else if (codeEnum == CodeEnum.PATCH_200) {
+                assertThat(possibleCause).isNotEqualTo(CodeEnum.PATCH_200.getDescription());
+            } else {
+                assertThat(possibleCause).isEqualTo("");
+            }
+        }
+    }
+
+    @Test
+    public void testPOST() {
+        for (CodeEnum codeEnum : CodeEnum.values()) {
+            int n = codeEnum.getNumber();
+            String possibleCause = ResponseStatusCodes.getPossibleCause("POST", n);
+            if (codeEnum.name().contains("POST")) {
+                if (n == 400) {
+                    assertThat(possibleCause).isEqualTo(CodeEnum.DELETE_GET_PATCH_POST_PUT_400.getDescription());
+                } else if (n == 403) {
+                    assertThat(possibleCause).isEqualTo(CodeEnum.DELETE_GET_PATCH_POST_PUT_403.getDescription());
+                } else if (n == 404) {
+                    assertThat(possibleCause).isEqualTo(EXPECTED_POST_404);
+                } else if (n == 405) {
+                    assertThat(possibleCause).isEqualTo(CodeEnum.DELETE_GET_PATCH_POST_PUT_405.getDescription());
+                } else if (n == 406) {
+                    assertThat(possibleCause).isEqualTo(EXPECTED_POST_406);
+                } else if (n == 415) {
+                    assertThat(possibleCause).isEqualTo(EXPECTED_POST_415);
+                } else if (n == 500) {
+                    assertThat(possibleCause).isEqualTo(CodeEnum.DELETE_GET_PATCH_POST_PUT_500.getDescription());
+                } else {
+                    assertThat(possibleCause).isEqualTo("");
+                }
+            } else {
+                assertThat(possibleCause).isEqualTo("");
+            }
+        }
+    }
+}
